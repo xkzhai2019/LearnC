4e4f0894aeaef3a6072050ca0a8b90c8d747e018
xkzhai_Home_WSL <xkzhai@Home.WSL>
2024-02-14 07:15:42 +0800

    scanf invalid input

---
+++ 061scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int i1 = 1, i2 = 2, i3 = 3, i4 = 4;
+    scanf("%d",&i1); 
+    scanf("%d",&i2);
+    printf("i1+i2=%d\n",i1+i2);
+    scanf("%d %d",&i3,&i4);
+    printf("i3+i4=%d\n",i3+i4);
+
+    return 0;
+}
