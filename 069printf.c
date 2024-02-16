4331ca09c66de2b0e440665cf77d27d567f5872f
xkzhai_Home_WSL <xkzhai@Home.WSL>
2024-02-16 08:17:53 +0800

    printf buffer

---
+++ 069printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void){
+
+    //printf("苏州科技大学");
+    printf("苏州科技大学\n");
+    sleep(5); // 程序暂停5秒
+    printf("程序设计与算法语言\n");
+    return 0;
+}
