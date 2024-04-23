f3df23ca5bb11027c91dcd28f8508b60a0a70785
Home-ubuntu <Home@ubuntu.com>
2024-04-23 14:59:49 +0800

    stack

--- 167func.c
+++ 167func.c
@@ -2,15 +2,15 @@
 
 // 计算阶乘，递归实现
 int factorial(int n){
-    printf("计算%d的阶乘\n",n);
+//    printf("计算%d的阶乘\n",n);
     if(n==0 || n==1 ){
         return 1;
     }
     return n * factorial(n-1);
 }
 int main(void){
-    printf("2!=%d\n",factorial(2));
-    printf("3!=%d\n",factorial(3));
+  //  printf("2!=%d\n",factorial(2));
+  //  printf("3!=%d\n",factorial(3));
     printf("4!=%d\n",factorial(4));
     return 0;
 }
