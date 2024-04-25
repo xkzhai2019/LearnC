ca25e6693c09943ec56e16651976363b9a565ae2
REN9000k <xkzhai@REN9000k.com>
2024-04-25 09:41:12 +0800

    func declear

--- 165func.c
+++ 165func.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int sum(int m, int n);// 计算正整数m到n的和
-
+int sum(int m, int n);
+/* 计算正整数m到n的和,函数声明
+*/
 int main(void){
     printf("1~100的和为%d\n",sum(1,100));
     return 0;
@@ -9,7 +10,8 @@ int main(void){
 
 int sum(int m, int n){
     int ret = 0;
-    for(int i=m;i<=n;i++){
+    int i;
+    for(i=m;i<=n;i++){
         ret += i;
     }
     return ret;
